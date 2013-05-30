883515698f185a86722aeb680ab88140720c186a
bobzurad <bob@zurad.net>
2013-05-29 22:23:32 -0500

    css, html, js, jquery 2.0.1

--- js/default.js
+++ js/default.js
@@ -8,11 +8,43 @@
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
+    var $original = null;
+    var $final = null;
+    var $temperature = null;
+    var $calibration = null;
+    var $result = null;
+
+    var onCalcClick = function (e) {
+        $result.val(131 * (parseFloat($original.val(), 10) - parseFloat($final.val(), 10)));
+    };
+
+    var setupDropDowns = function () {
+        for (var i = 59; i <= 90; i++) {
+            $temperature.append($("<option></option>")
+                .attr("value", i)
+                .text(i));
+
+            $calibration.append($("<option></option>")
+                .attr("value", i)
+                .text(i));
+        }
+    };
+
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
                 // your application here.
+
+                $original = $("#original");
+                $final = $("#final");
+                $temperature = $("#temperature");
+                $calibration = $("#calibration");
+                $result = $("#result");
+
+                $("#calc").on("click", onCalcClick);
+
+                setupDropDowns();
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
